5ad54ecc50ef34da7c5c305c17844d8fd34c2a96
Frosmin <simonabastomart@gmail.com>
2024-08-27 15:19:44 -0400

    commit 2

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    
+    use HasFactory;    
     protected $fillable = ['title', 'content'];
 }
 // pueba 
+// prueba 2
