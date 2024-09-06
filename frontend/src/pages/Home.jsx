9eaf9ff66d82c26323933a2ecb81eef36c80e6a4
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-05 23:09:43 -0400

    pruebas home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
@@ -27,150 +28,229 @@ import {
   AccordionDetails,
   Tooltip,
   CircularProgress,
-} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import './css/Home.css';
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import "./css/Home.css";
 
 const Home = () => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [dropdownValue, setDropdownValue] = useState('');
+  const [dropdownValue, setDropdownValue] = useState("");
   const [checked, setChecked] = useState(true);
-  const [radioValue, setRadioValue] = useState('option1');
+  const [radioValue, setRadioValue] = useState("option1");
 
   const handleMenuOpen = (event) => {
-      setAnchorEl(event.currentTarget);
+    setAnchorEl(event.currentTarget);
   };
 
   const handleMenuClose = () => {
-      setAnchorEl(null);
+    setAnchorEl(null);
   };
 
   const handleDropdownChange = (event) => {
-      setDropdownValue(event.target.value);
+    setDropdownValue(event.target.value);
   };
 
   const handleCheckboxChange = (event) => {
-      setChecked(event.target.checked);
+    setChecked(event.target.checked);
   };
 
   const handleRadioChange = (event) => {
-      setRadioValue(event.target.value);
+    setRadioValue(event.target.value);
   };
 
   return (
-      <div>
-          {/* AppBar Example */}
-          <AppBar position="static">
-              <Toolbar>
-                  <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
-                      <MenuIcon />
-                  </IconButton>
-                  <Typography variant="h6" style={{ flexGrow: 1 }}>
-                      Material UI Examples
-                  </Typography>
-                  <Button color="inherit">Login</Button>
-              </Toolbar>
-          </AppBar>
+    <div>
+      {/* AppBar Example */}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Material UI Examples
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
 
-          {/* Dropdown Menu */}
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-              <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-              <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-              <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
-          </Menu>
+      {/* Dropdown Menu */}
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+      </Menu>
 
-          {/* Main Container */}
-          <Container style={{ marginTop: '20px' }}>
-              <Typography variant="h4" gutterBottom>
-                  Explore Material UI Components
-              </Typography>
+      {/* Main Container */}
+      <Container style={{ marginTop: "20px" }}>
+        <Typography variant="h4" gutterBottom>
+          Explore Material UI Components
+        </Typography>
+        {/* Navigation Buttons */}
+        <Box sx={{ mb: 4 }}>
+          <Button
+            component={Link}
+            to="/registrodocente"
+            variant="contained"
+            color="primary"
+            sx={{ mr: 2 }}
+          >
+            Registro Docente
+          </Button>
+          <Button
+            component={Link}
+            to="/registroestudiante"
+            variant="contained"
+            color="primary"
+            sx={{ mr: 2 }}
+          >
+            Registro Estudiante
+          </Button>
+          <Button
+            component={Link}
+            to="/registroperiodoacademico"
+            variant="contained"
+            color="primary"
+          >
+            Registro Período Académico
+          </Button>
+        </Box>
+        {/* Card Example */}
+        <Card style={{ marginBottom: "20px" }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              Card Example
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              This is an example of a card component using Material UI.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+            <IconButton color="secondary">
+              <FavoriteIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
 
-              {/* Card Example */}
-              <Card style={{ marginBottom: '20px' }}>
-                  <CardContent>
-                      <Typography variant="h5" component="div">
-                          Card Example
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                          This is an example of a card component using Material UI.
-                      </Typography>
-                  </CardContent>
-                  <CardActions>
-                      <Button size="small">Learn More</Button>
-                      <IconButton color="secondary">
-                          <FavoriteIcon />
-                      </IconButton>
-                  </CardActions>
-              </Card>
+        {/* Form Examples */}
+        <Box component="form" noValidate autoComplete="off" sx={{ mb: 4 }}>
+          <TextField
+            fullWidth
+            label="Outlined"
+            variant="outlined"
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Filled"
+            variant="filled"
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Standard"
+            variant="standard"
+            margin="normal"
+          />
+          <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+            Submit
+          </Button>
+        </Box>
 
-              {/* Form Examples */}
-              <Box component="form" noValidate autoComplete="off" sx={{ mb: 4 }}>
-                  <TextField fullWidth label="Outlined" variant="outlined" margin="normal" />
-                  <TextField fullWidth label="Filled" variant="filled" margin="normal" />
-                  <TextField fullWidth label="Standard" variant="standard" margin="normal" />
-                  <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-                      Submit
-                  </Button>
-              </Box>
+        {/* Select / Dropdown Example */}
+        <FormControl fullWidth sx={{ mb: 4 }}>
+          <InputLabel>Select an Option</InputLabel>
+          <Select
+            value={dropdownValue}
+            label="Select an Option"
+            onChange={handleDropdownChange}
+          >
+            <MenuItem value="option1">Option 1</MenuItem>
+            <MenuItem value="option2">Option 2</MenuItem>
+            <MenuItem value="option3">Option 3</MenuItem>
+          </Select>
+        </FormControl>
 
-              {/* Select / Dropdown Example */}
-              <FormControl fullWidth sx={{ mb: 4 }}>
-                  <InputLabel>Select an Option</InputLabel>
-                  <Select value={dropdownValue} label="Select an Option" onChange={handleDropdownChange}>
-                      <MenuItem value="option1">Option 1</MenuItem>
-                      <MenuItem value="option2">Option 2</MenuItem>
-                      <MenuItem value="option3">Option 3</MenuItem>
-                  </Select>
-              </FormControl>
+        {/* Checkbox and Radio Example */}
+        <FormControlLabel
+          control={
+            <Checkbox checked={checked} onChange={handleCheckboxChange} />
+          }
+          label="Check me"
+        />
 
-              {/* Checkbox and Radio Example */}
-              <FormControlLabel
-                  control={<Checkbox checked={checked} onChange={handleCheckboxChange} />}
-                  label="Check me"
-              />
+        <FormControl component="fieldset" sx={{ mb: 4 }}>
+          <RadioGroup value={radioValue} onChange={handleRadioChange}>
+            <FormControlLabel
+              value="option1"
+              control={<Radio />}
+              label="Option 1"
+            />
+            <FormControlLabel
+              value="option2"
+              control={<Radio />}
+              label="Option 2"
+            />
+            <FormControlLabel
+              value="option3"
+              control={<Radio />}
+              label="Option 3"
+            />
+          </RadioGroup>
+        </FormControl>
 
-              <FormControl component="fieldset" sx={{ mb: 4 }}>
-                  <RadioGroup value={radioValue} onChange={handleRadioChange}>
-                      <FormControlLabel value="option1" control={<Radio />} label="Option 1" />
-                      <FormControlLabel value="option2" control={<Radio />} label="Option 2" />
-                      <FormControlLabel value="option3" control={<Radio />} label="Option 3" />
-                  </RadioGroup>
-              </FormControl>
+        {/* Slider Example */}
+        <Typography gutterBottom>Slider Example</Typography>
+        <Slider
+          defaultValue={30}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+          sx={{ mb: 4 }}
+        />
 
-              {/* Slider Example */}
-              <Typography gutterBottom>Slider Example</Typography>
-              <Slider defaultValue={30} aria-label="Default" valueLabelDisplay="auto" sx={{ mb: 4 }} />
+        {/* Switch Example */}
+        <FormControlLabel
+          control={<Switch checked={checked} onChange={handleCheckboxChange} />}
+          label="Toggle me"
+        />
 
-              {/* Switch Example */}
-              <FormControlLabel control={<Switch checked={checked} onChange={handleCheckboxChange} />} label="Toggle me" />
+        {/* Accordion Example */}
+        <Accordion sx={{ mb: 4 }}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              This is an accordion detail section where you can place any
+              content.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
 
-              {/* Accordion Example */}
-              <Accordion sx={{ mb: 4 }}>
-                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                      <Typography>Accordion 1</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                      <Typography>
-                          This is an accordion detail section where you can place any content.
-                      </Typography>
-                  </AccordionDetails>
-              </Accordion>
+        {/* Tooltip Example */}
+        <Tooltip title="Delete">
+          <IconButton>
+            <FavoriteIcon />
+          </IconButton>
+        </Tooltip>
 
-              {/* Tooltip Example */}
-              <Tooltip title="Delete">
-                  <IconButton>
-                      <FavoriteIcon />
-                  </IconButton>
-              </Tooltip>
-
-              {/* Circular Progress Example */}
-              <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-                  <CircularProgress />
-              </Box>
-          </Container>
-      </div>
+        {/* Circular Progress Example */}
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+          <CircularProgress />
+        </Box>
+      </Container>
+    </div>
   );
 };
 
