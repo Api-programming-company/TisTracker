2be5c4c3498fe8e3e996181b191db5adba484a1e
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-13 19:35:35 -0400

    agregar validaciones de correo y sus test

--- frontend/src/utils/validaciones.jsx
+++ frontend/src/utils/validaciones.jsx
@@ -3,3 +3,13 @@ export const validarContraseña = (contraseña) => {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/|\\~`-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;"'<>,.?/|\\~`-]{8,}$/;
   return regex.test(contraseña);
 };
+
+export const validarEmailDocente = (email) => {
+  const regexDocente = /^[a-zA-Z0-9._%+-]+@fcyt\.umss\.edu\.bo$/;
+  return regexDocente.test(email);
+};
+
+export const validarEmailEstudiante = (email) => {
+  const regexEstudiante = /^[0-9]{9}@est\.umss\.edu$/;
+  return regexEstudiante.test(email);
+};
