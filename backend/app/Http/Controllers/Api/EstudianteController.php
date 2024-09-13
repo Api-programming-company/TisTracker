5001df4f946551ffdab83905835dd76ff209bdb3
Frosmin <simonabastomart@gmail.com>
2024-09-13 12:56:29 -0400

    añadiendo validar password

--- backend/app/Http/Controllers/Api/EstudianteController.php
+++ backend/app/Http/Controllers/Api/EstudianteController.php
@@ -25,7 +25,7 @@ class EstudianteController extends Controller
             'apellido' => 'required|string|max:255',
             'email' => 'required|email|unique:estudiantes,email',
             'codSis' => 'required|integer|unique:estudiantes,codSis',
-            'password' => 'required|string|min:8', // Se requiere una contraseña de al menos 8 caracteres
+            'password' => 'required|string|min:8', 
         ]);
 
         $estudiante = Estudiante::create($validatedData);
