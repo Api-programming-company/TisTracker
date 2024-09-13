d42f1490bf6043d4211a6f3cad2f06ae160c6666
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-13 11:44:11 -0400

    quitar componentes de registros individuales

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,5 +1,3 @@
-export { default as RegistroDocente } from "./RegistroDocente";
-export { default as RegistroEstudiante } from "./RegistroEstudiante";
 export { default as Sidebar } from "./Sidebar";
 export { default as TestComponent } from "./testComponent";
 export { default as ListaPeriodosAcademicos } from "./ListaPeriodosAcademicos";
