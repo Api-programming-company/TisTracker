cd342556225cc46afd095989203ede6042f383f4
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-14 11:33:33 -0400

    agregar first_name y last_name al user

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -12,13 +12,15 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -26,21 +28,26 @@ class AuthController extends Controller
         return response()->json(['message' => 'User registered successfully'], 201);
     }
 
+
     public function login(Request $request)
     {
+        // Validar email y password
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
 
+        // Buscar al usuario por email
         $user = User::where('email', $request->email)->first();
 
+        // Verificar si las credenciales son incorrectas
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
 
+        // Crear un token de acceso para el usuario autenticado
         $token = $user->createToken('api-token')->plainTextToken;
 
         return response()->json(['token' => $token], 200);
@@ -48,6 +55,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        // Revocar todos los tokens del usuario autenticado
         $request->user()->tokens()->delete();
 
         return response()->json(['message' => 'Logged out'], 200);
@@ -55,6 +63,7 @@ class AuthController extends Controller
 
     public function user(Request $request)
     {
+        // Retornar la información del usuario autenticado
         return response()->json($request->user());
     }
 }
