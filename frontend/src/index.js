171106d97a73a4e0bf2096cc0720e7d5aafd5b60
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-15 23:57:57 -0400

    corregir strict mode para el desarrollo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,24 @@ import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+reportWebVitals();
+
+/*
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./app/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
@@ -19,3 +37,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
