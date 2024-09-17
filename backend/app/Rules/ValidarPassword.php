970057c0795aa3fac77efd0a8a95dca5d47f2d48
Frosmin <simonabastomart@gmail.com>
2024-09-17 18:25:21 -0400

    modificando validadores de contraseñas

--- backend/app/Rules/ValidarPassword.php
+++ backend/app/Rules/ValidarPassword.php
@@ -9,11 +9,11 @@ class ValidarPassword implements Rule
     protected $nombre;
     protected $apellidos;
 
-    public function __construct($nombre, $apellidos)
-    {
-        $this->nombre = $nombre;
-        $this->apellidos = $apellidos;
-    }
+    // public function __construct($nombre, $apellidos)
+    // {
+    //     $this->nombre = $nombre;
+    //     $this->apellidos = $apellidos;
+    // }
 
     public function passes($attribute, $value)
     {
@@ -22,18 +22,18 @@ class ValidarPassword implements Rule
         $hasLowerCase = preg_match('/[a-z]/', $value); // Al menos una minúscula
         $hasNumber = preg_match('/\d/', $value);// Al menos un número
         $hasSpecialChar = preg_match('/[!@#$%^&*]/', $value);// Al menos un caracter especial
-        $hasNoSpaces = !preg_match('/\s/', $value);// No puede contener espacios
-        $noCommonSequences = !preg_match('/(12345|abcdef|qwerty)/', $value);// No puede contener secuencias comunes
-        $noNameParts = !preg_match('/' . preg_quote($this->nombre, '/') . '|' . preg_quote($this->apellidos, '/') . '/i', $value);// No puede contener el nombre o apellidos
+        //$hasNoSpaces = !preg_match('/\s/', $value);// No puede contener espacios
+        //$noCommonSequences = !preg_match('/(12345|abcdef|qwerty)/', $value);// No puede contener secuencias comunes
+        //$noNameParts = !preg_match('/' . preg_quote($this->nombre, '/') . '|' . preg_quote($this->apellidos, '/') . '/i', $value);// No puede contener el nombre o apellidos
 
         return strlen($value) >= $minLength &&
                $hasUpperCase &&
                $hasLowerCase &&
                $hasNumber &&
-               $hasSpecialChar &&
-               $hasNoSpaces &&
-               $noCommonSequences &&
-               $noNameParts;
+               $hasSpecialChar;
+               //$hasNoSpaces ;
+              // $noCommonSequences &&
+              // $noNameParts;
     }
 
     public function message()
