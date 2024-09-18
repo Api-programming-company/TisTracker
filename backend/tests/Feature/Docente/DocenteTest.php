a8379abce5583d35771f38908b4ae6102bca9749
Frosmin <simonabastomart@gmail.com>
2024-09-18 00:11:05 -0400

    tets verificar si el password es tiene numero

--- backend/tests/Feature/Docente/DocenteTest.php
+++ backend/tests/Feature/Docente/DocenteTest.php
@@ -399,6 +399,42 @@ class AuthControllerTest extends TestCase
          Mail::assertNotSent(VerifyEmail::class);
      }
 
+       /** @test */
+       public function Password_tiene_numero()
+       {
+           // Simular el envío de correo
+           Mail::fake();
+   
+           $response = $this->postJson('/api/user/register', [
+               'first_name' => 'simon',
+               'last_name' => 'pepe',
+               'email' => '123456789@fcyt.umss.edu.bo',
+               'password' => 'Password@',    // Menor a 8 caracteres
+               'password_confirmation' => 'Password@',
+               'user_type' => 'D',
+           ]);
+   
+           // Verificar que la respuesta tenga un estado 422 (Unprocessable Entity)
+           $response->assertStatus(422);
+           $response->assertJson([
+               'message' => 'Error de validación'
+           ]);
+   
+           // Verificar que el usuario no fue creado en la base de datos
+           $this->assertDatabaseMissing('users', [
+               'email' => '123456789@fcyt.umss.edu.bo',
+           ]);
+   
+           // Verificar que no se creó un token de verificación en la base de datos
+           $user = User::where('email', '123456789@fcyt.umss.edu.bo')->first();
+           $this->assertNull($user);
+   
+           // Verificar que no se envió el correo de verificación
+           Mail::assertNotSent(VerifyEmail::class);
+       }
+
+    
+
 }
 
 
