70959cf454fa713cc88879e365a3d0a27fb682a8
Frosmin <simonabastomart@gmail.com>
2024-09-18 19:48:25 -0400

    crear periodo acadenico test nombre obligatorio

--- backend/tests/Feature/PeriodoAcademico/PeriodoAcademicoTest.php
+++ backend/tests/Feature/PeriodoAcademico/PeriodoAcademicoTest.php
@@ -78,4 +78,36 @@ class AuthControllerDocenteTest extends TestCase
             'user_id' => $docente->id,
         ]);
     }
+
+
+
+
+    //Campos obligatorios
+
+     /** @test */
+     public function nombre_obligatorio()
+     {
+         // Crear un usuario con tipo Estudiante "E"
+         $estudiante = User::factory()->create(['user_type' => 'D']);
+         $this->actingAs($estudiante);
+ 
+         // Datos de prueba para el periodo académico
+         $data = [
+             'name' => '',
+             'start_date' => '2023-01-01',
+             'end_date' => '2023-06-30',
+             'description' => 'Descripción del periodo 1',
+         ];
+ 
+         // Enviar la solicitud para crear el periodo académico
+         $response = $this->postJson('/api/docente/academic-periods', $data);
+ 
+         // Verificar que la respuesta sea 422 campo obligatorio
+         $response->assertStatus(422);
+ 
+         // Verificar que el periodo académico no fue creado en la base de datos
+         $this->assertDatabaseMissing('academic_periods', [
+             'name' => 'Periodo 1',
+         ]);
+     }
 }
