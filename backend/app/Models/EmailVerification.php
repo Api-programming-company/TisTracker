11d3e75f4f51cc28524bac4e645cb0adf2be6cb8
Daye0210 <202202138@est.umss.edu>
2024-09-20 14:45:03 -0400

    ya no es necesario

--- backend/app/Models/EmailVerification.php
+++ backend/app/Models/EmailVerification.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+/*
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,3 +21,4 @@ class EmailVerification extends Model
         return $this->belongsTo(User::class);
     }
 }
+*/
