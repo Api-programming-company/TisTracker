d2b1f38bbcc19bbbffddd85e8fed8636654ae52d
Daye0210 <202202138@est.umss.edu>
2024-09-20 14:50:36 -0400

    Cambiando estado y reenviando una vista despues de verificar

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes(['verify' => true]);
+
+Route::get('/home', function() {
+    return view('emails.dashboard');
+});
