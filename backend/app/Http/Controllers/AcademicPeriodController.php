dd06296351c3325d06a9468a4c45b3212445845f
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-21 15:45:25 -0400

    validaciones registro de periodo academico

--- backend/app/Http/Controllers/AcademicPeriodController.php
+++ backend/app/Http/Controllers/AcademicPeriodController.php
@@ -6,6 +6,8 @@ use App\Models\AcademicPeriod;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Exception;
+use Illuminate\Validation\ValidationException;
 
 class AcademicPeriodController extends Controller
 {
@@ -28,29 +30,49 @@ class AcademicPeriodController extends Controller
 
     public function store(Request $request)
     {
-        $user = Auth::user();
+        try {
+            $user = Auth::user();
 
-        if ($user->user_type !== 'D') {
-            return response()->json(['error' => 'Unauthorized'], 403);
+            // Verificar el tipo de usuario
+            if ($user->user_type !== 'D') {
+                return response()->json(['error' => 'Unauthorized'], 403);
+            }
+
+            // Validar la solicitud
+            $request->validate([
+                'name' => 'required|string|max:255|unique:academic_periods',
+                'start_date' => 'required|date',
+                'end_date' => 'required|date|after:start_date',
+                'description' => 'nullable|string',
+            ]);
+
+            // Crear el periodo académico
+            $academicPeriod = AcademicPeriod::create([
+                'name' => $request->name,
+                'start_date' => $request->start_date,
+                'end_date' => $request->end_date,
+                'description' => $request->description,
+                'user_id' => $user->id,
+            ]);
+
+            // Devolver respuesta de éxito
+            return response()->json($academicPeriod, 201);
+        } catch (ValidationException $e) {
+            // Manejar errores de validación
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $e->validator->errors()
+            ], 422); // 422 Unprocessable Entity
+
+        } catch (Exception $e) {
+            // Manejar otros errores
+            return response()->json([
+                'message' => 'An error occurred',
+                'error' => $e->getMessage()
+            ], 500); // 500 Internal Server Error
         }
-
-        $request->validate([
-            'name' => 'required|string|max:255|unique:academic_periods',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after:start_date',
-            'description' => 'nullable|string',
-        ]);
-
-        $academicPeriod = AcademicPeriod::create([
-            'name' => $request->name,
-            'start_date' => $request->start_date,
-            'end_date' => $request->end_date,
-            'description' => $request->description,
-            'user_id' => $user->id,
-        ]);
-
-        return response()->json($academicPeriod, 201);
     }
+
     public function getAllGroupedByTeacher()
     {
         $academicPeriods = AcademicPeriod::with('creator')->get();
@@ -73,25 +95,24 @@ class AcademicPeriodController extends Controller
     {
         /** @var \App\Models\User $user **/
         $user = Auth::user();
-    
+
         if ($user->user_type !== 'E') {
             return response()->json(['message' => 'Solo estudiantes pueden inscribirse.'], 403);
         }
-    
+
         // Verifica que el estudiante no esté ya inscrito en un periodo académico
         if ($user->academic_period_id) {
             return response()->json(['message' => 'Ya está inscrito en un periodo académico'], 400);
         }
-    
+
         $academicPeriodId = $request->input('academic_period_id'); // Obtén el ID desde el cuerpo de la solicitud
-    
+
         $academicPeriod = AcademicPeriod::findOrFail($academicPeriodId);
-    
+
         // Inscribe al estudiante en el periodo académico
         $user->academic_period_id = $academicPeriod->id;
         $user->save();
-    
+
         return response()->json(['message' => 'Se inscribió correctamente en el periodo académico']);
     }
-    
 }
--- frontend/src/pages/RegistroPeriodoAcademico.jsx
+++ frontend/src/pages/RegistroPeriodoAcademico.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   TextField,
   Button,
@@ -9,43 +9,111 @@ import {
   CircularProgress,
 } from "@mui/material";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
-import { format } from "date-fns";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { useCreateAcademicPeriodMutation } from "../api/academicPeriodApi";
 import { useNavigate } from "react-router-dom";
+import { differenceInDays } from "date-fns";
 
 const RegistroPeriodoAcademico = () => {
+  const MAX_DESCRIPTION_LENGTH = 255;
+  const MIN_DAYS_DIFFERENCE = 3;
   const [name, setName] = useState("");
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState({});
+  const [nameError, setNameError] = useState(false);
 
-  const [createAcademicPeriod, { isLoading, error }] =
+  const [createAcademicPeriod, { data, error, isLoading, isError, isSuccess }] =
     useCreateAcademicPeriodMutation();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (isSuccess) {
+      console.log(data);
+      navigate("/academic-periods")
+    }
+    if (isError) {
+      if (error.data?.errors?.name) {
+        //setErrors(error.data.errors || {});
+        setNameError(true);
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          name: "El nombre ya esta en uso",
+        }));
+      }
+      console.log(error);
+    }
+  }, [isSuccess, isError, error, data]);
+
+  const handleDescriptionChange = (e) => {
+    const value = e.target.value;
+    if (value.length <= MAX_DESCRIPTION_LENGTH) {
+      setDescription(value);
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        description: undefined,
+      }));
+    }
+  };
+
+  const handleNameChange = (e) => {
+    setNameError(false);
+    setName(e.target.value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      name: undefined,
+    }));
+  };
+
+  const handleStartDateChange = (newValue) => {
+    setStartDate(newValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      startDate: undefined,
+    }));
+  };
+
+  const handleEndDateChange = (newValue) => {
+    setEndDate(newValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      endDate: undefined,
+    }));
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setErrors({});
 
-    // Verifica que todos los campos obligatorios estén llenos
-    if (!name || !startDate || !endDate) {
-      alert("Por favor, completa todos los campos obligatorios.");
+    const newErrors = {};
+    if (!name) newErrors.name = "Este campo es obligatorio.";
+    if (!startDate) newErrors.startDate = "Este campo es obligatorio.";
+    if (!endDate) newErrors.endDate = "Este campo es obligatorio.";
+    if (description.length > MAX_DESCRIPTION_LENGTH)
+      newErrors.description = `La descripción no puede exceder ${MAX_DESCRIPTION_LENGTH} caracteres.`;
+
+    // Validación de fechas
+    if (startDate && endDate) {
+      if (startDate > endDate) {
+        newErrors.startDate =
+          "La fecha de inicio no puede ser mayor que la fecha de fin.";
+      } else if (differenceInDays(endDate, startDate) < MIN_DAYS_DIFFERENCE) {
+        newErrors.endDate = `La diferencia mínima debe ser de ${MIN_DAYS_DIFFERENCE} días.`;
+      }
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
       return;
     }
 
-    try {
-      await createAcademicPeriod({
-        name,
-        start_date: startDate,
-        end_date: endDate,
-        description,
-      }).unwrap();
-      // Redirige a la lista de períodos académicos después de registrar
-      navigate("/academic-periods");
-    } catch (err) {
-      // Maneja el error
-      alert(`Error: ${err.message}`);
-    }
+    createAcademicPeriod({
+      name,
+      start_date: startDate,
+      end_date: endDate,
+      description,
+    });
   };
 
   return (
@@ -67,22 +135,28 @@ const RegistroPeriodoAcademico = () => {
               label="Nombre"
               variant="outlined"
               fullWidth
-              required
-              helperText="* Obligatorio"
+              error={!!errors.name}
+              helperText={errors.name || "* Obligatorio"}
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleNameChange}
+              disabled={isLoading}
             />
           </FormControl>
 
-          {/* Descripción (Opcional) */}
+          {/* Descripción */}
           <FormControl fullWidth sx={{ mb: 2 }}>
             <TextField
-              label="Descripción (Opcional)"
+              label="Descripción"
               variant="outlined"
               fullWidth
-              helperText="Opcional"
+              error={!!errors.description}
+              helperText={
+                errors.description ||
+                `Máximo ${MAX_DESCRIPTION_LENGTH} caracteres.`
+              }
               value={description}
-              onChange={(e) => setDescription(e.target.value)}
+              onChange={handleDescriptionChange}
+              disabled={isLoading}
             />
           </FormControl>
 
@@ -99,28 +173,28 @@ const RegistroPeriodoAcademico = () => {
                 <DatePicker
                   label="Fecha Inicio"
                   value={startDate}
-                  onChange={(newValue) => setStartDate(newValue)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      helperText="* Obligatorio"
-                      fullWidth
-                    />
-                  )}
+                  onChange={handleStartDateChange}
+                  slotProps={{
+                    textField: {
+                      error: !!errors.startDate,
+                      helperText: errors.startDate || "DD/MM/AAAA",
+                    },
+                  }}
                   format="dd/MM/yyyy"
+                  disabled={isLoading}
                 />
                 <DatePicker
                   label="Fecha Fin"
                   value={endDate}
-                  onChange={(newValue) => setEndDate(newValue)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      helperText="* Obligatorio"
-                      fullWidth
-                    />
-                  )}
+                  onChange={handleEndDateChange}
+                  slotProps={{
+                    textField: {
+                      error: !!errors.endDate,
+                      helperText: errors.endDate || "DD/MM/AAAA",
+                    },
+                  }}
                   format="dd/MM/yyyy"
+                  disabled={isLoading}
                 />
               </Box>
             </LocalizationProvider>
@@ -133,16 +207,10 @@ const RegistroPeriodoAcademico = () => {
             color="primary"
             fullWidth
             sx={{ mt: 2 }}
-            disabled={isLoading} // Deshabilita el botón durante la carga
+            disabled={isLoading || nameError}
           >
             {isLoading ? <CircularProgress size={24} /> : "Registrar Período"}
           </Button>
-
-          {error && (
-            <Typography color="error" sx={{ mt: 2 }}>
-              Error: {error.message}
-            </Typography>
-          )}
         </form>
       </Box>
     </Container>
