af4a100802f043f3834b73efa2a5b59591829c17
Frosmin <simonabastomart@gmail.com>
2024-09-22 01:28:03 -0400

    test preuab correo 4

--- backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
+++ backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
@@ -545,4 +545,31 @@ class RegistrarGrupoEmpresaTest extends TestCase
         // Verificar la respuesta
         $response->assertStatus(422);
     }
+
+     /** @test */
+     public function preuba_correo_sinPuntoCom()
+     {
+         // Crear un usuario y un periodo académico
+         $academicPeriod = AcademicPeriod::factory()->create();
+         $user = User::factory()->create(['academic_period_id' => $academicPeriod->id]);
+ 
+         // Autenticar al usuario usando Sanctum
+         Sanctum::actingAs($user);
+ 
+         
+         $data = [
+             'long_name' => 'EmpPrbsad',
+             'short_name' => 'EmpPrb',
+             'email' => 'hola@gmail',
+             'address' => '123 Calle Principal',
+             'phone' => '12345678',
+             'members' => [$user->id],
+         ];
+ 
+         // Realizar la solicitud para crear la compañía
+         $response = $this->postJson('/api/company', $data);
+ 
+         // Verificar la respuesta
+         $response->assertStatus(422);
+     }
 }
