a07eef3bdd83efe3559a6786f9f24976f4f8d8c7
Frosmin <simonabastomart@gmail.com>
2024-09-22 01:22:47 -0400

    test preuab correo

--- backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
+++ backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
@@ -464,4 +464,31 @@ class RegistrarGrupoEmpresaTest extends TestCase
         $response->assertJsonValidationErrors('email');
     }
 
+
+    /** @test */
+    public function preuba_correo_sinprefijo()
+    {
+        // Crear un usuario y un periodo académico
+        $academicPeriod = AcademicPeriod::factory()->create();
+        $user = User::factory()->create(['academic_period_id' => $academicPeriod->id]);
+
+        // Autenticar al usuario usando Sanctum
+        Sanctum::actingAs($user);
+
+        // Datos de la solicitud sin 'long_name'
+        $data = [
+            'long_name' => 'EmpPrbsad',
+            'short_name' => 'EmpPrb',
+            'email' => '@gmial.com',
+            'address' => '123 Calle Principal',
+            'phone' => '12345678',
+            'members' => [$user->id],
+        ];
+
+        // Realizar la solicitud para crear la compañía
+        $response = $this->postJson('/api/company', $data);
+
+        // Verificar la respuesta
+        $response->assertStatus(422);
+    }
 }
