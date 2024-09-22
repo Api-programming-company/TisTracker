e0bb418a6d1222532bb637970e65b7d081fbe7a6
Frosmin <simonabastomart@gmail.com>
2024-09-22 00:55:04 -0400

    test registrar Grupo empresa 2

--- backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
+++ backend/tests/Feature/Company/RegistrarGrupoEmpresaTest.php
@@ -209,4 +209,30 @@ class RegistrarGrupoEmpresaTest extends TestCase
         // Verificar la respuesta
         $response->assertStatus(422);
     }
+
+     /** @test */
+     public function short_name_es_obligatorio()
+     {
+         // Crear un usuario y un periodo académico
+         $academicPeriod = AcademicPeriod::factory()->create();
+         $user = User::factory()->create(['academic_period_id' => $academicPeriod->id]);
+ 
+         // Autenticar al usuario usando Sanctum
+         Sanctum::actingAs($user);
+ 
+         // Datos de la solicitud sin 'short_name'
+         $data = [
+             'long_name' => 'Empresa de Prueba',
+             'email' => 'empresa@prueba.com',
+             'address' => '123 Calle Principal',
+             'phone' => '12345678',
+             'members' => [$user->id],
+         ];
+ 
+         // Realizar la solicitud para crear la compañía
+         $response = $this->postJson('/api/company', $data);
+ 
+         // Verificar la respuesta
+         $response->assertStatus(422);
+     }
 }
