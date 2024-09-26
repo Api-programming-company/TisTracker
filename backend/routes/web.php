025cb5ddc56e1fa6a6e54f8b2d3f80a8658f9658
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-26 00:41:33 -0400

    Revert "primer deployado"
    This reverts commit 00853c6b1c0162d2c659f4c201eaa4a8fa5839c7.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
-Route::get('/{any}', function () {
-    return view('index');
-})->where('any', '^(?!api).*$');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes(['verify' => true]);
 
-Route::get('/home', function () {
+Route::get('/home', function() {
     return view('emails.dashboard');
 });
