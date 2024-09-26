555a43e8a1fa0a51854b5ea9a4e766e43857272e
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-26 19:39:00 -0400

    quitar informacion de la planificacion de la empresa

--- frontend/src/pages/PlannificationRegister.jsx
+++ frontend/src/pages/PlannificationRegister.jsx
@@ -1,13 +1,23 @@
 import { Box } from "@mui/material";
 import { useParams } from "react-router-dom";
+import CompanyPlanning from "../components/company/CompanyPlanning";
+import { useState } from "react";
 
 const PlannificationRegister = () => {
-    const { id } = useParams();
-    return (
-        <Box>
-            {id}
-        </Box>
-    );
-}
+  const { id } = useParams();
+  const [formData, setFormData] = useState({
+    milestones: [],
+  });
+  const [sendData, setSendData] = useState({
+    milestones: [],
+  });
+  return (
+    <CompanyPlanning
+      milestones={formData.milestones}
+      setFormData={setFormData}
+      setSendData={setSendData}
+    />
+  );
+};
 
 export default PlannificationRegister;
