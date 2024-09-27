fb7ddcc9f7cc2a31b47cd2548d69d70a72420797
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-27 00:57:53 -0400

    agregar ejemplos

--- frontend/src/components/company/AcceptDeclineCompany.jsx
+++ frontend/src/components/company/AcceptDeclineCompany.jsx
@@ -3,6 +3,38 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 const AcceptDeclineCompany = () => {
+  const invitacionesEjemplo = {
+    message: "Compañías pendientes obtenidas correctamente.",
+    company: {
+      id: 33,
+      long_name: "asdfasasdasf",
+      short_name: "asdas",
+      email: "asdasd@sadas.com",
+      address: "sdasdasd",
+      phone: "77665453",
+      academic_period_id: 1,
+      created_at: "2024-09-24T05:58:41.000000Z",
+      updated_at: "2024-09-27T02:56:01.000000Z",
+      status: "A",
+      members_count: 2,
+      members: [
+        {
+          user_id: 8,
+          email: "123456789@est.umss.edu",
+          pivot: {
+            company_id: 33,
+            user_id: 8,
+            status: "P",
+            permission: "W",
+            created_at: "2024-09-24T05:58:42.000000Z",
+            updated_at: "2024-09-24T05:58:42.000000Z",
+          },
+        },
+      ],
+    },
+    invitation_date: "2024-09-24T05:58:42.000000Z",
+  };
+
   const company = useLocation().state.request;
   return (
     <Box sx={{ mt: 5, mb: 10 }}>
--- frontend/src/pages/InvitacionesGE.jsx
+++ frontend/src/pages/InvitacionesGE.jsx
@@ -16,6 +16,72 @@ import { useGetPendingCompaniesRequestQuery } from "../api/studentApi";
 import { useUpdateInvitationByCompanyIdMutation } from "../api/invitationApi";
 
 const InvitacionesGE = () => {
+  const invitacionesEjemplo = {
+    message: "Compañías pendientes obtenidas correctamente.",
+    companies: [
+      {
+        company: {
+          id: 14,
+          long_name: "Empresa de simon",
+          short_name: "simcorp",
+          email: "simon@gmail.com",
+          address: "umss",
+          phone: "12345678",
+          academic_period_id: 13,
+          created_at: "2024-09-24T01:13:08.000000Z",
+          updated_at: "2024-09-24T01:25:43.000000Z",
+          status: "A",
+          members_count: 4,
+          members: [
+            {
+              user_id: 8,
+              email: "123456789@est.umss.edu",
+              pivot: {
+                company_id: 14,
+                user_id: 8,
+                status: "P",
+                permission: "R",
+                created_at: "2024-09-24T01:13:10.000000Z",
+                updated_at: "2024-09-24T01:13:10.000000Z"
+              }
+            }
+          ]
+        },
+        invitation_date: "2024-09-24T01:13:10.000000Z"
+      },
+      {
+        company: {
+          id: 33,
+          long_name: "asdfasasdasf",
+          short_name: "asdas",
+          email: "asdasd@sadas.com",
+          address: "sdasdasd",
+          phone: "77665453",
+          academic_period_id: 1,
+          created_at: "2024-09-24T05:58:41.000000Z",
+          updated_at: "2024-09-27T02:56:01.000000Z",
+          status: "A",
+          members_count: 2,
+          members: [
+            {
+              user_id: 8,
+              email: "123456789@est.umss.edu",
+              pivot: {
+                company_id: 33,
+                user_id: 8,
+                status: "P",
+                permission: "W",
+                created_at: "2024-09-24T05:58:42.000000Z",
+                updated_at: "2024-09-24T05:58:42.000000Z"
+              }
+            }
+          ]
+        },
+        invitation_date: "2024-09-24T05:58:42.000000Z"
+      }
+    ]
+  };
+  
   const { data, error, isSuccess, isFetching, isError } =
     useGetPendingCompaniesRequestQuery();
 
