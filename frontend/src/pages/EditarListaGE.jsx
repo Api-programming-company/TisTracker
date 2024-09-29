68a294eb4d42d1928b34fbce332fd2183b58aa45
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-29 18:25:26 -0400

    corregir retirar invitaciones

--- frontend/src/pages/EditarListaGE.jsx
+++ frontend/src/pages/EditarListaGE.jsx
@@ -16,93 +16,71 @@ import {
 } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 import { Person } from "@mui/icons-material";
-import { useCreateCompanyMutation } from "../api/companyApi";
+import { format, parseISO } from 'date-fns';
+
 import DialogMod from "../components/DialogMod";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useGetCompanyByIdQuery } from "../api/companyApi";
 
 const EditarListaGE = () => {
+  const { id } = useParams();
   const navigate = useNavigate();
-  const [openA, setOpenA] = useState(false);
-  const [{ isSuccess, isError, isLoading }] = useCreateCompanyMutation();
-  const [itemIdToRemove, setItemIdToRemove] = useState(null);
 
+  const {
+    data: companyData,
+    error: companyError,
+    isSuccess: isCompanySuccess,
+    isError: isCompanyError,
+    isLoading: isCompanyLoading,
+    isFetching: isCompanyFetching,
+  } = useGetCompanyByIdQuery(id);
   const [selectedItems, setSelectedItems] = useState([]);
 
-  //Lista de invitados que aun no han aceptado
-  const [allToAcceptItems] = useState([
-    {
-      id: 3,
-      name: "Daniela Torrico Torreón",
-      fechainvitacion: "07/12/2021",
-      correo: "daniela@example.com",
-      estado: "Aceptado",
-    },
-    {
-      id: 4,
-      name: "Andres Castillo Lozada",
-      fechainvitacion: "07/12/2021",
-      correo: "andres@example.com",
-      estado: "Aceptado",
-    },
-    {
-      id: 5,
-      name: "Antonio Gomez Amaranto",
-      fechainvitacion: "07/12/2021",
-      correo: "antonio@example.com",
-    },
-    {
-      id: 6,
-      name: "Camila Torrez Gutierrez",
-      fechainvitacion: "07/12/2021",
-      correo: "camila@example.com",
-    },
-  ]);
+  useEffect(() => {
+    if (isCompanySuccess) {
+      console.log(companyData);
+      const pending = companyData.company.members.filter((member) => {
+        return member.pivot.status === "P";
+      });
+      setSelectedItems(pending);
+    }
+    if (isCompanyError) console.log(companyError);
+  }, [companyData, companyError, isCompanySuccess, isCompanyError]);
 
+  const [openA, setOpenA] = useState(false);
+  const [itemIdToRemove, setItemIdToRemove] = useState(null);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
 
-  useEffect(() => {
-    setSelectedItems((prevItems) => [...prevItems, ...allToAcceptItems]);
-
-    if (isSuccess) {
-      setSnackbarMessage("Lista de integrantes enviada correctamente");
-      setSnackbarOpen(true);
-      navigate("/");
-    }
-
-    if (isError) {
-      setSnackbarMessage("Error al enviar la lista de integrantes");
-      setSnackbarOpen(true);
-    }
-  }, [isSuccess, isError, navigate, allToAcceptItems]);
-
-  const handleConfirmRemoveItem = (itemId) => {
-    setOpenA(false);
-    handleRemoveItem(itemId);
-  };
   const handleRemoveItem = (itemId) => {
     setSelectedItems((prevItems) =>
       prevItems.filter((item) => item.id !== itemId)
     );
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    {
-      /* if (condicion) {
-      setSnackbarMessage(
-        "Mensaje"
-      );
-      setSnackbarOpen(true);
-      return;
-    } */
-    }
-    alert("Invitaciones retiradas correctamente");
+    setSnackbarMessage("Invitación retirada correctamente");
+    setSnackbarOpen(true);
+    setOpenA(false)
   };
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
   };
+
+  if (isCompanyFetching) {
+    return (
+      <Container
+        maxWidth="sm"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+        }}
+      >
+        <CircularProgress />
+      </Container>
+    );
+  }
 
   return (
     <Container maxWidth="md">
@@ -116,96 +94,75 @@ const EditarListaGE = () => {
           Invitaciones Pendientes
         </Typography>
 
-        <form onSubmit={handleSubmit}>
-          <FormControl fullWidth sx={{ mb: 1 }}>
-            <Box
-              sx={{
-                display: { xs: "block", sm: "flex" },
-                justifyContent: "space-between",
-                gap: 2,
-              }}
-            >
-              <FormControl fullWidth>
-                <Typography variant="subtitle1">
-                  Lista de Invitaciones a Retirar:
-                </Typography>
-                <List>
-                  {selectedItems.map((item) => (
-                    <ListItem
-                      key={item.id}
-                      secondaryAction={
-                        <IconButton
-                          edge="end"
-                          aria-label="delete"
-                          onClick={() => {
-                            setItemIdToRemove(item.id);
-                            setOpenA(true);
-                          }}
-                        >
-                          <Delete />
-                        </IconButton>
-                      }
-                      sx={{
-                        backgroundColor: "info.gray",
-                        mb: 0.5,
-                      }}
-                    >
-                      <ListItemIcon>
-                        <Avatar>
-                          <Person />
-                        </Avatar>
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item.name}
-                        secondary={
-                          <Box>
-                            <Typography variant="body2">
-                              Correo: {item.correo}
-                            </Typography>
-                            <Typography variant="body2">
-                              Invitación realizada en: {item.fechainvitacion}
-                            </Typography>
-                          </Box>
-                        }
-                      />
-                    </ListItem>
-                  ))}
-                </List>
-                <DialogMod
-                  open={openA}
-                  setOpen={setOpenA}
-                  title={"Confirmar retiro de invitación"}
-                  content={"¿Estas seguro que deseas retirar esta invitación?"}
-                  onAccept={() => {
-                    if (itemIdToRemove !== null) {
-                      handleConfirmRemoveItem(itemIdToRemove);
-                    }
-                  }}
-                />
-              </FormControl>
-            </Box>
-          </FormControl>
-
-          {/* Botón de Registro */}
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={isLoading}
+        <FormControl fullWidth sx={{ mb: 1 }}>
+          <Box
             sx={{
-              display: "block",
-              mx: "auto",
-              px: 12,
-              py: 1,
+              display: { xs: "block", sm: "flex" },
+              justifyContent: "space-between",
+              gap: 2,
             }}
           >
-            {isLoading ? (
-              <CircularProgress size={24} color="inherit" />
-            ) : (
-              "REALIZAR CAMBIOS"
-            )}
-          </Button>
-        </form>
+            <FormControl fullWidth>
+              <Typography variant="subtitle1">
+                Lista de Invitaciones a Retirar:
+              </Typography>
+              <List>
+                {selectedItems.map((item) => (
+                  <ListItem
+                    key={item.id}
+                    secondaryAction={
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => {
+                          setItemIdToRemove(item.id);
+                          setOpenA(true);
+                        }}
+                      >
+                        <Delete />
+                      </IconButton>
+                    }
+                    sx={{
+                      backgroundColor: "info.gray",
+                      mb: 0.5,
+                    }}
+                  >
+                    <ListItemIcon>
+                      <Avatar>
+                        <Person />
+                      </Avatar>
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={`${item.first_name} ${item.last_name}`}
+                      secondary={
+                        <Box>
+                          <Typography variant="body2">
+                            Correo: {item.email}
+                          </Typography>
+                          <Typography variant="body2">
+                            Invitación realizada en: {format(parseISO(item.created_at), 'dd/MM/yyyy HH:mm:ss')}
+                          </Typography>
+                        </Box>
+                      }
+                    />
+                  </ListItem>
+                ))}
+              </List>
+              <DialogMod
+                open={openA}
+                setOpen={setOpenA}
+                title={"Confirmar retiro de invitación"}
+                content={"¿Estas seguro que deseas retirar esta invitación?"}
+                onAccept={() => {
+                  if (itemIdToRemove !== null) {
+                    handleRemoveItem(itemIdToRemove);
+                    setItemIdToRemove(null);
+                  }
+                }}
+              />
+            </FormControl>
+          </Box>
+        </FormControl>
 
         {/* Snackbar para mensajes */}
         <Snackbar
