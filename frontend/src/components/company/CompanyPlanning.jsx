d81c8336b3724737c6940915b1a06a8355372c28
erwinparedez <erwin.erwin.ep@gmail.com>
2024-09-29 12:11:59 -0400

    Agregado de espaciado superior en el titulo para lista de planificacion de GE

--- frontend/src/components/company/CompanyPlanning.jsx
+++ frontend/src/components/company/CompanyPlanning.jsx
@@ -5,28 +5,24 @@ import { useUpdateCompanyPlanningByIdMutation } from "../../api/companyApi";
 import DialogMod from "../DialogMod";
 import { CiCirclePlus } from "react-icons/ci";
 
-
 const CompanyPlanning = ({ setFormData, setSendData }) => {
   //const [milestones, setMilestones] = useState([...milestones]); // Estado para los hitos en edición
   const [open, setOpen] = useState(false);
-  const [milestones,setMilestones] = useState([]);
-
+  const [milestones, setMilestones] = useState([]);
 
   const handleConfirm = () => {
     setFormData((prev) => ({
       ...prev,
       milestones: milestones, // Confirma los hitos temporales al formData
     }));
-    setSendData(true)
+    setSendData(true);
   };
 
-  const handleMilestoneChange = (updatedMilestone,milestone_id) => {
-    const updatedMilestones = milestones.map((milestone,index) =>{
-      console.log(milestone_id,index);
-      return milestone_id === index ? updatedMilestone : milestone
-    }
-      
-    );
+  const handleMilestoneChange = (updatedMilestone, milestone_id) => {
+    const updatedMilestones = milestones.map((milestone, index) => {
+      console.log(milestone_id, index);
+      return milestone_id === index ? updatedMilestone : milestone;
+    });
 
     setMilestones(updatedMilestones); // Actualiza la lista temporal
   };
@@ -43,20 +39,21 @@ const CompanyPlanning = ({ setFormData, setSendData }) => {
   };
 
   const handleDeleteMilestone = (id) => {
-    const updatedMilestones = milestones.filter((e)=> e.id !== id)
-    setMilestones(updatedMilestones)
-  }
+    const updatedMilestones = milestones.filter((e) => e.id !== id);
+    setMilestones(updatedMilestones);
+  };
 
   return (
     <div className="container">
-      
       <Typography
-            component="h1"
-            sx={{ color: "black", fontSize: "40px", lineHeight: "1" }}
-          >Planificación de equipo</Typography>
+        component="h1"
+        sx={{ color: "black", fontSize: "40px", lineHeight: "1" }}
+      >
+        Planificación de equipo
+      </Typography>
       <List>
         {milestones.length > 0 ? (
-          milestones.map((milestone,index) => (
+          milestones.map((milestone, index) => (
             <Milestone
               key={index}
               milestone_id={index}
@@ -69,50 +66,52 @@ const CompanyPlanning = ({ setFormData, setSendData }) => {
           <p>No hay hitos asignados</p>
         )}
       </List>
-      
-          
-          <Button
-            color="primary"
-            onClick={handleAddMilestone}
-            sx={{
-              backgroundColor: "transparent",
-              "&:hover": {
-                color: "white",
-                backgroundColor: "primary.dark",
-              },
-              mr: 2,
-              mb: 2,
-            }}
-          >
-            <i><CiCirclePlus /></i> Agregar Hito
-          </Button>
 
-          <Button
-            onClick={() => setOpen(true)}
-            sx={{
-              backgroundColor: "transparent",
-              border: "1px solid black",
-              "&:hover": {
-                color: "white",
-                backgroundColor: "primary.dark",
-              },
-              mb: 2,
-              mr: 2,
-            }}
-          >
-            Confirmar
-          </Button>
+      <Button
+        color="primary"
+        onClick={handleAddMilestone}
+        sx={{
+          backgroundColor: "transparent",
+          "&:hover": {
+            color: "white",
+            backgroundColor: "primary.dark",
+          },
+          mr: 2,
+          mb: 2,
+        }}
+      >
+        <i>
+          <CiCirclePlus />
+        </i>{" "}
+        Agregar Hito
+      </Button>
 
-          <DialogMod
-            open={open}
-            setOpen={setOpen}
-            title={"Confirmar"}
-            content={"¿Está seguro de realizar esta acción?"}
-            onAccept={handleConfirm}
-            onCancel={() => setOpen(false)}
-          />
+      <Button
+        onClick={() => setOpen(true)}
+        sx={{
+          backgroundColor: "transparent",
+          border: "1px solid black",
+          "&:hover": {
+            color: "white",
+            backgroundColor: "primary.dark",
+          },
+          mb: 2,
+          mr: 2,
+        }}
+      >
+        Confirmar
+      </Button>
+
+      <DialogMod
+        open={open}
+        setOpen={setOpen}
+        title={"Confirmar"}
+        content={"¿Está seguro de realizar esta acción?"}
+        onAccept={handleConfirm}
+        onCancel={() => setOpen(false)}
+      />
     </div>
-    );
+  );
 };
 
 export default CompanyPlanning;
