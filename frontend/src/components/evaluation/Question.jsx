04a2571e1993a406bc220ddeb3a478045ae3270e
erwinparedez <erwin.erwin.ep@gmail.com>
2024-10-04 12:59:33 -0400

    Corrección de colores para preguntas de las evaluaciones

--- frontend/src/components/evaluation/Question.jsx
+++ frontend/src/components/evaluation/Question.jsx
@@ -3,10 +3,7 @@ import React from "react";
 
 const Question = ({ question }) => {
   return (
-    <Typography
-        component="h3"
-        sx={{ color: "black", fontSize: "18px", lineHeight: "1" }}
-    >
+    <Typography component="h3" sx={{ fontSize: "18px", lineHeight: "1" }}>
       {question}
     </Typography>
   );
