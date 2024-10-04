351d17e139570672d194e4c75d20d1ba39c695a4
Jh0nZ <jhondeycraft776@gmail.com>
2024-10-04 16:45:29 -0400

    corregir nombre de tabla

--- backend/app/Models/CompanyUserEvaluation.php
+++ backend/app/Models/CompanyUserEvaluation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CompanyUserScore extends Model
+class CompanyUserEvaluation extends Model
 {
     use HasFactory;
 
--- backend/database/migrations/2024_10_04_003319_create_company_user_evaluations_table.php
+++ backend/database/migrations/2024_10_04_003319_create_company_user_evaluations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompanyUserScoresTable extends Migration
+class CreateCompanyUserEvaluationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCompanyUserScoresTable extends Migration
      */
     public function up()
     {
-        Schema::create('company_user_scores', function (Blueprint $table) {
+        Schema::create('company_user_evaluations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_user_id')->constrained('company_user')->onDelete('cascade');
             $table->foreignId('company_id')->constrained()->onDelete('cascade');
