9f6b1590f177f23a84020a97209fbd9f3f3a0a6d
Jh0nZ <jhondeycraft776@gmail.com>
2024-10-05 00:56:13 -0400

    deployar

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '^(?!api).*$');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
