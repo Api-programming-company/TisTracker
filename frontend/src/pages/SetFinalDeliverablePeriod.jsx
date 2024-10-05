604867c2321c474bb89be62faf45173fa035a77c
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-05 13:44:42 -0400

    Realizar verificaciones de los campos en Ajustar periodo de entrega final

--- frontend/src/pages/SetFinalDeliverablePeriod.jsx
+++ frontend/src/pages/SetFinalDeliverablePeriod.jsx
@@ -9,18 +9,43 @@ import "../styles/set_final_period.css";
 const SetFinalDeliverablePeriod = () => {
   const [startDate, setStartDate] = React.useState(null);
   const [endDate, setEndDate] = React.useState(null);
+  const [errors, setErrors] = React.useState({});
 
   const handleStartDateChange = (newValue) => {
     setStartDate(newValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      start_date: undefined,
+    }));
   };
 
   const handleEndDateChange = (newValue) => {
     setEndDate(newValue);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      end_date: undefined,
+    }));
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(startDate, endDate);
+    if (!startDate || !endDate) {
+      setErrors({
+        start_date: startDate ? undefined : "La fecha de inicio es requerida",
+        end_date: endDate ? undefined : "La fecha de fin es requerida",
+      });
+    } else if (endDate < startDate) {
+      setErrors({
+        end_date: "La fecha de fin debe ser mayor o igual que la fecha de inicio",
+      });
+    } else {
+      console.log(startDate, endDate);
+    }
+  };
+
+  const findError = (name) => {
+    const error = errors[name];
+    return error;
   };
 
   return (
@@ -34,26 +59,34 @@ const SetFinalDeliverablePeriod = () => {
                 <div className="dates-input-container">
                     <div className="date-item">
                         <DatePicker
+                            
                             label="Fecha de inicio"
                             value={startDate}
                             onChange={handleStartDateChange}
-                            renderInput={(params) => <TextField {...params} />}
-                        
+                            renderInput={(params) => <TextField
+                              {...params} 
+                             
+                            />}
+                            
                         />
-                        {/* {findError("start_date") && <p className="text-red-300 text-sm">{findError("start_date")}</p>} */}
-                        </div>
+                        {findError("start_date") && <p className="text-red-300 text-sm">{findError("start_date")}</p>}
+
+                    </div>
                         
                     <div className="date-item">
                             <DatePicker
                             label="Fecha de fin"
                             value={endDate}
                             onChange={handleEndDateChange}
-                            renderInput={(params) => <TextField {...params}  />}
+                            
+                            renderInput={(params) => <TextField
+                              {...params} 
+                            />}
                             
                         />
-                        {/* {findError("end_date") && <p className="text-red-300 text-sm">{findError("end_date")}</p>} */}
+                        {findError("end_date") && <p className="text-red-300 text-sm">{findError("end_date")}</p>}
 
-                        </div>
+                    </div>
                     
                 </div>
             </LocalizationProvider>
