d537d3042dc74896e53619ea834ac35ef4c426aa
Richi78 <ricardo.rojas.carvajal@gmail.com>
2024-10-06 21:42:41 -0300

    simbolo de * para preguntas obligatorias

--- frontend/src/components/evaluation/Question.jsx
+++ frontend/src/components/evaluation/Question.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Question = ({ question }) => {
   return (
     <Typography component="h3" sx={{ fontSize: "18px", lineHeight: "1" }}>
-      {question}
+      {question} *
     </Typography>
   );
 };
