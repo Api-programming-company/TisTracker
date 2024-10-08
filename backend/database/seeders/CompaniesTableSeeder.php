6278be4ef1a302c054466331e21f8dd1e36830cf
Daye0210 <202202138@est.umss.edu>
2024-10-07 21:32:48 -0400

    lo demas

--- backend/database/seeders/CompaniesTableSeeder.php
+++ backend/database/seeders/CompaniesTableSeeder.php
@@ -3,16 +3,34 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Company;
+use Carbon\Carbon;
 class CompaniesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        Company::create([
+            'long_name' => 'Company A',
+            'short_name' => 'C.A',
+            'email' => '111111111@est.umss.edu',
+            'address' => 'Av.Siempre Viva',
+            'phone' => '75698435',
+            'academic_period_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'status' => 'P',
+        ]);
+
+        Company::create([
+            'long_name' => 'Company B',
+            'short_name' => 'C.B',
+            'email' => '222222222@est.umss.edu',
+            'address' => 'Av.LIMA',
+            'phone' => '69453628',
+            'academic_period_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'status' => 'C',
+        ]);
     }
 }
