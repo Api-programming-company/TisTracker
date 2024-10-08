ae35e0393824f2fc91b676a98d7c682a549fb899
Daye0210 <202202138@est.umss.edu>
2024-10-07 21:05:08 -0400

    seeder para periodo academico

--- backend/database/seeders/AcademicPeriodsTableSeeder.php
+++ backend/database/seeders/AcademicPeriodsTableSeeder.php
@@ -3,16 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\AcademicPeriod;
 
 class AcademicPeriodsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        AcademicPeriod::create([
+            'id' => 1,
+            'name' => 'Primer Semestre 2024',
+            'start_date' => '2024-01-01',
+            'end_date' => '2024-06-30',
+            'teacher_id' => 5, // Docente BORIS MARCELO CALANCHA NAVIA
+        ]);
     }
 }
