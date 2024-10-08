cd95fef03332caa65d6002324cb35702a0b12a08
Daye0210 <202202138@est.umss.edu>
2024-10-07 21:19:58 -0400

    seeder para user datos compeltos

--- backend/database/seeders/UsersTableSeeder.php
+++ backend/database/seeders/UsersTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-
+use Carbon\Carbon;
 class UsersTableSeeder extends Seeder
 {
     public function run()
@@ -21,21 +21,42 @@ class UsersTableSeeder extends Seeder
 
         // Crear algunos usuarios para las pruebas
         User::create([
-            'name' => 'User 1',
+            'id' => 101,
+            'first_name' => 'User',
+            'last_name' => 'One',
             'email' => '123456789@est.umss.edu',
             'password' => bcrypt('password123'),
+            'user_type' => 'E', 
+            'academic_period_id' => 1, 
+            'email_verified_at' => Carbon::now(), 
+            'created_at' => Carbon::now(), 
+            'updated_at' => Carbon::now(), 
         ]);
 
         User::create([
-            'name' => 'User 2',
+            'id' => 102,
+            'first_name' => 'User',
+            'last_name' => 'Two',
             'email' => '113456789@est.umss.edu',
             'password' => bcrypt('password123'),
+            'user_type' => 'E', 
+            'academic_period_id' => 1, 
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         User::create([
-            'name' => 'User 3',
+            'id' => 103,
+            'first_name' => 'User',
+            'last_name' => 'Three',
             'email' => '122456789@est.umss.edu',
             'password' => bcrypt('password123'),
+            'user_type' => 'E',
+            'academic_period_id' => 1, 
+            'email_verified_at' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
     }
 }
