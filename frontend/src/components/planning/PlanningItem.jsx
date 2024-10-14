f2e000965284e0d6e31420036216d484026ca15e
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-14 11:12:23 -0400

    feat: agregar todos los datos a la planilla de seguimiento

--- frontend/src/components/planning/PlanningItem.jsx
+++ frontend/src/components/planning/PlanningItem.jsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import React from 'react'
 
 const PlanningItem = ({deliverable,index}) => {
@@ -9,11 +9,17 @@ const PlanningItem = ({deliverable,index}) => {
         </div>
         <div className="grid-item">{deliverable.name}</div>
         <div className="grid-item">
-        <input type="number" placeholder='0' value={0} className='grid-number-input'/>
+        <input type="number" placeholder='0' value={0}  min={0} max={100} className='grid-input number'/>
         </div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
+        <div className="grid-item">
+          <input type="number" placeholder='0' value={0} min={0} max={100} className='grid-input number'/>
+        </div>
+        <div className="grid-item">
+          <textarea name="observations" id="observations" className='grid-input area' ></textarea>
+        </div>
+        <div className="grid-item">
+          <Button>Carry Over</Button>
+        </div>
 
     </>
   )
