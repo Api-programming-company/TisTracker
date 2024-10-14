bc70ac3deb437597dce32c4fbbe31d59202db665
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-14 11:17:25 -0400

    Agregar un manejador de estados a la planilla de seguimiento

--- frontend/src/components/planning/PlanningItem.jsx
+++ frontend/src/components/planning/PlanningItem.jsx
@@ -1,7 +1,47 @@
 import { Button, TextField } from '@mui/material'
 import React from 'react'
+import { defer } from 'react-router-dom';
 
 const PlanningItem = ({deliverable,index}) => {
+  const [inputValue, setInputValue] = React.useState({
+    observedResult: 0,
+    hopeResult: 0,
+    observations: ""
+  });
+  const [buttonAction, setButtonAction] = React.useState("Carry Over");
+
+  const handleInputChange = (event) => {
+    switch (event.target.name) {
+      case "observedResult":
+        setInputValue((prevState) => ({...prevState, observedResult: parseInt(event.target.value)}));
+        break;
+      case "hopeResult":
+        setInputValue((prevState) => ({...prevState, hopeResult: parseInt(event.target.value)}));
+        break;
+      case "observations":
+        setInputValue((prevState) => ({...prevState, observations: event.target.value}));
+        break;
+      default:
+        console.log("This state doesn't exist")
+        break;
+    }
+  };
+
+  const handleActionButton = () => {
+    switch (buttonAction) {
+      case "Carry Over":
+        setButtonAction("Saving...");
+        //TODO: implement the logic to save the information in the database
+        setTimeout(() => {
+          setButtonAction("Carry Over");
+        }, 2000);
+        break;
+        default:
+          console.log("This state doesn't exist");
+          break;
+    }
+  };
+
   return (
     <>
         <div className="grid-item">
@@ -9,16 +49,16 @@ const PlanningItem = ({deliverable,index}) => {
         </div>
         <div className="grid-item">{deliverable.name}</div>
         <div className="grid-item">
-        <input type="number" placeholder='0' value={0}  min={0} max={100} className='grid-input number'/>
+          <input type="number" placeholder='0' value={inputValue.observedResult} onChange={handleInputChange} name="observedResult" min={0} max={100} className='grid-input number'/>
         </div>
         <div className="grid-item">
-          <input type="number" placeholder='0' value={0} min={0} max={100} className='grid-input number'/>
+          <input type="number" placeholder='0' value={inputValue.hopeResult} onChange={handleInputChange} name="hopeResult" min={0} max={100} className='grid-input number'/>
         </div>
         <div className="grid-item">
-          <textarea name="observations" id="observations" className='grid-input area' ></textarea>
+          <textarea name="observations" placeholder="Ponga sus observaciones aqui" id="observations" className='grid-input area' value={inputValue.observations} onChange={handleInputChange} ></textarea>
         </div>
         <div className="grid-item">
-          <Button>Carry Over</Button>
+          <Button onClick={handleActionButton}>{buttonAction}</Button>
         </div>
 
     </>
