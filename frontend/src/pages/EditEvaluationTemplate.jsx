6ba9c3340d309b25e9ec2321f5c9c81357ee8a94
Richi78 <ricardo.rojas.carvajal@gmail.com>
2024-10-18 16:46:26 -0300

    recuperar info de una evaluacion para EditEvaluationTemplate

--- frontend/src/pages/EditEvaluationTemplate.jsx
+++ frontend/src/pages/EditEvaluationTemplate.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useGetCompanyQuestionsByIdQuery } from "../api/evaluationApi";
 
 const EditEvaluationTemplate = () => {
   const { evaluation_id } = useParams();
 
-    
-  return (
-    <div>EditEvaluationTemplate</div>
-  )
-}
+  const {
+    data: companyQuestions,
+    isSuccess: companyQuestionsSuccess,
+    isFetching: companyQuestionsFetching,
+    isError: isCompanyQuestionsError,
+    error: companyQuestionsError,
+  } = useGetCompanyQuestionsByIdQuery(evaluation_id);
+  useEffect(() => {
+    if (companyQuestionsSuccess) {
+      console.log(companyQuestions);
+    //   setInitialState(companyQuestions);
+    }
+    if (isCompanyQuestionsError) {
+      console.log(companyQuestionsError);
+    }
+  }, [
+    companyQuestions,
+    companyQuestionsFetching,
+    isCompanyQuestionsError,
+    companyQuestionsError,
+    companyQuestionsSuccess,
+  ]);
+  return <div>EditEvaluationTemplate</div>;
+};
 
-export default EditEvaluationTemplate
+export default EditEvaluationTemplate;
