eaadc878ac2f19a127e6bc574c19190de45b09f8
erwinparedez <erwin.erwin.ep@gmail.com>
2024-10-25 18:25:09 -0400

    Agregado de titulo y cambio de nombres en el home de Estudiante

--- frontend/src/components/student/StudentHome.jsx
+++ frontend/src/components/student/StudentHome.jsx
@@ -1,56 +1,72 @@
 import React, { useContext } from "react";
-import { Container, Typography, Paper, Box } from "@mui/material";
+import { Container, Typography, Paper, Box, Divider } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../../context/AppContext";
 
 const StudentHome = () => {
-    const { user } = useContext(AppContext);
-    const navigate = useNavigate();
+  const { user } = useContext(AppContext);
+  const navigate = useNavigate();
 
-    const handleNavigate = (path) => {
-        navigate(path);
-    };
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
 
-    const menuItems = [
-        { label: user.company ? "Mi empresa" : "Crear empresa", path: user.company ? `/vergrupoe/${user.company.company_id}` : "/registroge" },
-        { label: "Empresas", path: `/academic-period/${user.academic_period_id}/companies/` },
-        { label: "Invitaciones", path: "/company-requests" }
-    ];
+  const menuItems = [
+    {
+      label: user.company ? "Mi Grupo Empresa" : "Crear Grupo Empresa",
+      path: user.company
+        ? `/vergrupoe/${user.company.company_id}`
+        : "/registroge",
+    },
+    //{ label: "Empresas", path: `/academic-period/${user.academic_period_id}/companies/` },
+    { label: "Invitaciones", path: "/invitations-home" },
+  ];
 
-    return (
-        <Container>
-            <Box
-                display="flex"
-                justifyContent="space-between"
-                flexWrap="wrap"
-                gap={3}
-            >
-                {user.academic_period_id ? (
-                    menuItems.map((item, index) => (
-                        <Box key={index} flexBasis={{ xs: "100%", md: "30%" }}>
-                            <Paper
-                                elevation={3}
-                                style={{ padding: "16px", cursor: "pointer" }}
-                                onClick={() => handleNavigate(item.path)}
-                            >
-                                <Typography variant="h5">{item.label}</Typography>
-                            </Paper>
-                        </Box>
-                    ))
-                ) : (
-                    <Box flexBasis={{ xs: "100%", md: "30%" }}>
-                        <Paper
-                            elevation={3}
-                            style={{ padding: "16px", cursor: "pointer" }}
-                            onClick={() => handleNavigate("/enroll-to-ap")}
-                        >
-                            <Typography variant="h5">Enroll to Academic Period</Typography>
-                        </Paper>
-                    </Box>
-                )}
+  console.log(user);
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 12 }}>
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          Taller de Ingeniería de Software
+        </Typography>
+      </Box>
+      <Divider sx={{ width: "100%", mb: 4 }} />{" "}
+      <Box display="flex" justifyContent="start" flexWrap="wrap" gap={3}>
+        {user.academic_period_id ? (
+          menuItems.map((item, index) => (
+            <Box key={index} flexBasis={{ xs: "100%", md: "30%" }}>
+              <Paper
+                elevation={3}
+                style={{
+                  padding: "16px",
+                  cursor: "pointer",
+                  paddingTop: "32px",
+                  paddingBottom: "32px",
+                  paddingLeft: "16px",
+                  paddingRight: "16px",
+                  textAlign: "center",
+                }}
+                onClick={() => handleNavigate(item.path)}
+              >
+                <Typography variant="h5">{item.label}</Typography>
+              </Paper>
             </Box>
-        </Container>
-    );
+          ))
+        ) : (
+          <Box flexBasis={{ xs: "100%", md: "30%" }}>
+            <Paper
+              elevation={3}
+              style={{ padding: "16px", cursor: "pointer" }}
+              onClick={() => handleNavigate("/enroll-to-ap")}
+            >
+              <Typography variant="h5">Enroll to Academic Period</Typography>
+            </Paper>
+          </Box>
+        )}
+      </Box>
+    </Container>
+  );
 };
 
 export default StudentHome;
