de7a75dca2647a1e1b03edf0566c89eedf75f460
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-27 18:21:54 -0400

    fix: visibilidad de la tabla en dark mode

--- frontend/src/components/GridComponent.jsx
+++ frontend/src/components/GridComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import "../styles/custom_grid.css"
@@ -6,9 +6,14 @@ import "../styles/custom_grid.css"
 const GridComponent = ({values}) => {
     const n_columns = Object.keys(values[0]).length
     
+    useEffect(() => {
+        console.log(localStorage.getItem("darkMode"));
+    },[])
+
+
     return (
         <div className="custom-grid" style={{gridTemplateColumns: `repeat(${n_columns},1fr)`}}>
-            {Object.keys(values[0]).map((header,index) => <div className='custom-grid-header' style={{backgroundColor: localStorage.getItem('theme') === 'dark' ? '#f00' : '#dadada'}} key={index}>{header}</div>)}
+            {Object.keys(values[0]).map((header,index) => <div className='custom-grid-header' style={{backgroundColor: localStorage.getItem('darkMode') ? '#555' : '#dadada'}} key={index}>{header}</div>)}
             {values.map((row,i) => Object.values(row).map((item,j) => <div className='custom-grid-item'  key={`${i}${j}`}>{item}</div>))}
         </div>
         
