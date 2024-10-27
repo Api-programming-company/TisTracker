bf723f9cc75217794525e56a7835fb4d9af0763f
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-27 17:36:07 -0400

    Separar la funcion para descargar archivos csv en Utils

--- frontend/src/pages/StudentsReport.jsx
+++ frontend/src/pages/StudentsReport.jsx
@@ -1,48 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Button } from '@mui/material'
 import { generateValues } from '../mock_objects/reporte'
+import GridComponent from '../components/GridComponent'
+import { downloadCsv } from '../utils/toCsv'
 const StudentsReport = () => {
     const {data} = generateValues();
+    //booleano para ver si los datos estan listos para mostrarse
+    const [finalData,setFinalData] = useState([])
+    const [readyToPrint, setReadyToPrint] = useState(false);
 
-    const transformToCSV = () => {
-        const headers = [...Object.keys(data[0]),"Evaluaciones","Total"];
-        const rows = data.map(row => {
-            const {autoevaluacion,pares,cruzada,planificacion} = row;
-            const totalEvaluations = Math.round((autoevaluacion + pares + cruzada) / 3)
-            const total = (totalEvaluations * 40/100) + (planificacion * 60/100)
+    
 
-            return [...Object.values(row), totalEvaluations, total].join(',')
-
-        }).join('\n');
-
-        return [headers.join(','), rows].join('\n');
-    }
-    const download = () => {
-        const csv = transformToCSV();
-        console.log(csv);
-
-        // Crear un Blob con los datos separados por comas en csv
-        const blob = new Blob([csv], {type: 'text/csv'});
-
-        // Crear una url para el Blob
-        const url = URL.createObjectURL(blob);
-
-
-        // Crear un anchor para descargar
-        const a = document.createElement("a")
-
-        // Definir la url y el attributo para descargar
-        a.href = url;
-        a.download = 'reporte_de_estudiantes.csv'
-
-        // Lanzar la descarga
-        a.click();
-    }
+    useEffect(() => {
+        // Agregar los datos: Promedio de evaluacions y Total
+        if(!finalData.length){
+            setFinalData(data.map((row) => {
+                const {autoevaluacion,pares,cruzada,planificacion} = row;
+                const totalEvaluations = Math.round((autoevaluacion + pares + cruzada) / 3)
+                const total = (totalEvaluations * 40/100) + (planificacion * 60/100)
+                return {...row, evaluaciones : totalEvaluations, total}
+            }))
+        }
+        
+    },[data,finalData])
 
 
   return (
     <Box display="flex" justifyContent="center" alignItems="center" marginTop={20}>
-        <Button onClick={download}>Descargar</Button>
+        <GridComponent></GridComponent>
+        <Button onClick={() => downloadCsv(finalData,"reporte_de_estudiantes")}>Descargar</Button>
     </Box>
   )
 }
---
+++ frontend/src/utils/toCsv.js
@@ -0,0 +1,30 @@
+const transformToCSV = (data) => {
+    console.log(data);
+    const headers = Object.keys(data[0]);
+    const rows = data.map(row => Object.values(row).join(',')).join('\n');
+
+    return [headers.join(','), rows].join('\n');
+}
+const downloadCsv = (data,name) => {
+    const csv = transformToCSV(data);
+    console.log(csv);
+
+    // Crear un Blob con los datos separados por comas en csv
+    const blob = new Blob([csv], {type: 'text/csv'});
+
+    // Crear una url para el Blob
+    const url = URL.createObjectURL(blob);
+
+
+    // Crear un anchor para descargar
+    const a = document.createElement("a")
+
+    // Definir la url y el attributo para descargar
+    a.href = url;
+    a.download = `${name.trim()}.csv`
+
+    // Lanzar la descarga
+    a.click();
+}
+
+export {downloadCsv}
