c80cdac7d51cff90c80a0caae3e905174a3233fc
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-30 18:56:15 -0400

    fix: solucionar bugs al descargar el pdf con el nombre incorrecto

--- frontend/src/components/ReportTemplate.jsx
+++ frontend/src/components/ReportTemplate.jsx
@@ -4,43 +4,46 @@ import GridComponent from './GridComponent'
 import { downloadCsv } from '../utils/toCsv'
 import generatePDF, { Resolution, Margin } from 'react-to-pdf';
 
-const options = {
-    // default is `save`
-    method: 'save',
-    // default is Resolution.MEDIUM = 3, which should be enough, higher values
-    // increases the image quality but also the size of the PDF, so be careful
-    // using values higher than 10 when having multiple pages generated, it
-    // might cause the page to crash or hang.
-    resolution: Resolution.MEDIUM,
-    page: {
-       // margin is in MM, default is Margin.NONE = 0
-       margin: Margin.MEDIUM,
-       // default is 'A4'
-       format: 'letter',
-       // default is 'portrait'
-       orientation: 'portrait',
-    },
-    canvas: {
-       // default is 'image/jpeg' for better size performance
-       mimeType: 'image/png',
-       qualityRatio: 1
-    },
-    // Customize any value passed to the jsPDF instance and html2canvas
-    // function. You probably will not need this and things can break, 
-    // so use with caution.
-    overrides: {
-       // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
-       pdf: {
-          compress: true
-       },
-       // see https://html2canvas.hertzen.com/configuration for more options
-       canvas: {
-          useCORS: true
-       }
-    },
- };
+
 
 const ReportTemplate = ({data,title,filename}) => {
+
+    const options = {
+        filename: `${filename}.pdf`,
+        // default is `save`
+        method: 'save',
+        // default is Resolution.MEDIUM = 3, which should be enough, higher values
+        // increases the image quality but also the size of the PDF, so be careful
+        // using values higher than 10 when having multiple pages generated, it
+        // might cause the page to crash or hang.
+        resolution: Resolution.MEDIUM,
+        page: {
+           // margin is in MM, default is Margin.NONE = 0
+           margin: Margin.MEDIUM,
+           // default is 'A4'
+           format: 'letter',
+           // default is 'portrait'
+           orientation: 'portrait',
+        },
+        canvas: {
+           // default is 'image/jpeg' for better size performance
+           mimeType: 'image/png',
+           qualityRatio: 1
+        },
+        // Customize any value passed to the jsPDF instance and html2canvas
+        // function. You probably will not need this and things can break, 
+        // so use with caution.
+        overrides: {
+           // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+           pdf: {
+              compress: true
+           },
+           // see https://html2canvas.hertzen.com/configuration for more options
+           canvas: {
+              useCORS: true
+           }
+        },
+     };
 
     const targetRef = useRef();
 
@@ -66,7 +69,7 @@ const ReportTemplate = ({data,title,filename}) => {
         }}>
             Descargar como csv
         </Button>
-        <Button onClick={() => generatePDF(targetRef,options, {filename: `${filename}.pdf`})} sx={{
+        <Button onClick={() => generatePDF(targetRef,options)} sx={{
             backgroundColor:"primary.main",
             color: "white"
         }}>
