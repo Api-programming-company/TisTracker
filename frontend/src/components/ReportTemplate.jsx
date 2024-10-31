ce0a3ccce634b9cdde4b737541cc6877c147a23b
Kevin Huayllas <KevinOmega01@gmail.com>
2024-10-31 15:19:12 -0400

    refactor: Agregar una funcion que devulve las opciones de la descarga en pdf

--- frontend/src/components/ReportTemplate.jsx
+++ frontend/src/components/ReportTemplate.jsx
@@ -3,47 +3,13 @@ import { Box,Button,Typography } from '@mui/material'
 import GridComponent from './GridComponent'
 import { downloadCsv } from '../utils/toCsv'
 import generatePDF, { Resolution, Margin } from 'react-to-pdf';
+import { getOptions } from '../utils/pdfOptions';
 
 
 
 const ReportTemplate = ({data,title,filename}) => {
 
-    const options = {
-        filename: `${filename}.pdf`,
-        // default is `save`
-        method: 'save',
-        // default is Resolution.MEDIUM = 3, which should be enough, higher values
-        // increases the image quality but also the size of the PDF, so be careful
-        // using values higher than 10 when having multiple pages generated, it
-        // might cause the page to crash or hang.
-        resolution: Resolution.MEDIUM,
-        page: {
-           // margin is in MM, default is Margin.NONE = 0
-           margin: Margin.MEDIUM,
-           // default is 'A4'
-           format: 'letter',
-           // default is 'portrait'
-           orientation: 'portrait',
-        },
-        canvas: {
-           // default is 'image/jpeg' for better size performance
-           mimeType: 'image/png',
-           qualityRatio: 1
-        },
-        // Customize any value passed to the jsPDF instance and html2canvas
-        // function. You probably will not need this and things can break, 
-        // so use with caution.
-        overrides: {
-           // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
-           pdf: {
-              compress: true
-           },
-           // see https://html2canvas.hertzen.com/configuration for more options
-           canvas: {
-              useCORS: true
-           }
-        },
-     };
+    const options = getOptions(filename);
 
     const targetRef = useRef();
 
--- frontend/src/pages/PlanningReport.jsx
+++ frontend/src/pages/PlanningReport.jsx
@@ -4,50 +4,16 @@ import { downloadCsv } from '../utils/toCsv';
 import generatePDF, { Resolution, Margin } from 'react-to-pdf';
 import { useGetPlanningByCompanyIdQuery } from '../api/planningApi';
 import { useParams } from 'react-router-dom';
-
+import { getOptions } from '../utils/pdfOptions';
 
 const PlanningReport = () => {
 
     const {id} = useParams();
     const { data, isSuccess, isFetching, isError, error } =
     useGetPlanningByCompanyIdQuery(id);
+    const options = getOptions("reporte_de_evaluacion_semanal")
 
-    const options = {
-        filename: `Reporte_de_evaluacion_semanal.pdf`,
-        // default is `save`
-        method: 'save',
-        // default is Resolution.MEDIUM = 3, which should be enough, higher values
-        // increases the image quality but also the size of the PDF, so be careful
-        // using values higher than 10 when having multiple pages generated, it
-        // might cause the page to crash or hang.
-        resolution: Resolution.MEDIUM,
-        page: {
-           // margin is in MM, default is Margin.NONE = 0
-           margin: Margin.MEDIUM,
-           // default is 'A4'
-           format: 'letter',
-           // default is 'portrait'
-           orientation: 'portrait',
-        },
-        canvas: {
-           // default is 'image/jpeg' for better size performance
-           mimeType: 'image/png',
-           qualityRatio: 1
-        },
-        // Customize any value passed to the jsPDF instance and html2canvas
-        // function. You probably will not need this and things can break, 
-        // so use with caution.
-        overrides: {
-           // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
-           pdf: {
-              compress: true
-           },
-           // see https://html2canvas.hertzen.com/configuration for more options
-           canvas: {
-              useCORS: true
-           }
-        },
-     };
+    
 
 
     const filename = "Reporte de evaluaciones semanales"
