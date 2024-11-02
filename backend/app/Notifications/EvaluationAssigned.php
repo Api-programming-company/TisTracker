946d0817b8d40cc1f7361d085c054ec6649786b5
Daye0210 <202202138@est.umss.edu>
2024-11-01 23:43:59 -0400

    manejo de fecha y hora con Carbon para facilitar el manejo

--- backend/app/Notifications/EvaluationAssigned.php
+++ backend/app/Notifications/EvaluationAssigned.php
@@ -3,45 +3,49 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Carbon\Carbon;
 
 class EvaluationAssigned extends Notification
 {
     use Queueable;
 
     public $evaluationName;
+    public $evaluationType;
+    public $startDate;
+    public $endDate;
 
-    public function __construct($evaluationName)
+    public function __construct($evaluationName, $evaluationType, $startDate, $endDate)
     {
         $this->evaluationName = $evaluationName;
-    }
+        $this->evaluationType = $evaluationType;
 
-    public function via($notifiable)
-    {
-        return ['mail']; // u otros canales de notificación, como 'database', 'slack', etc.
+        $startDate = $startDate instanceof Carbon ? $startDate : Carbon::parse($startDate);
+        $endDate = $endDate instanceof Carbon ? $endDate : Carbon::parse($endDate);
+
+        // Formatear fecha y hora por separado
+        $this->startDate = [
+            'date' => $startDate->format('d/m/Y'),
+            'time' => $startDate->format('H:i')
+        ];
+
+        $this->endDate = [
+            'date' => $endDate->format('d/m/Y'),
+            'time' => $endDate->format('H:i')
+        ];
     }
 
     public function toMail($notifiable)
     {
         return (new MailMessage)
+                    ->greeting('¡Buenas!')
                     ->subject('Nueva evaluación asignada')
                     ->line("Has recibido una nueva evaluación: {$this->evaluationName}")
-                    ->action('Ver Evaluación', url('/'))
+                    ->line("Tipo de evaluación: {$this->evaluationType}")
+                    ->line("Fecha de inicio: {$this->startDate['date']}Hora de inicio: {$this->startDate['time']}")
+                    ->line("Fecha de fin: {$this->endDate['date']} Hora de fin: {$this->endDate['time']}")
                     ->line('¡Buena suerte!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
-    }
 }
