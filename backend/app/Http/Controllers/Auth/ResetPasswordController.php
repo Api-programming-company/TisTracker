05d860b72492a285467ffcc8c279fefca6f3b1e0
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-11-03 13:00:42 -0400

    agregar clases para que funcione php artisan route:list

---
+++ backend/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\VerifiesEmails;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
+class ResetPasswordController extends Controller
+{
+
+}
