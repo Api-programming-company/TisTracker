fd985e848201c5adb9bdbcedf529149a211a7541
Daye0210 <202202138@est.umss.edu>
2024-12-08 12:29:06 -0400

    add columna evaluacion

---
+++ backend/database/migrations/2024_12_08_162759_add_evaluation_dates_to_academic_period_evaluations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEvaluationDatesToAcademicPeriodEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('academic_period_evaluations', function (Blueprint $table) {
+            $table->date('evaluation_start_date');
+            $table->date('evaluation_end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('academic_period_evaluations', function (Blueprint $table) {
+            $table->dropColumn(['evaluation_start_date', 'evaluation_end_date']);
+        });
+    }
+}
